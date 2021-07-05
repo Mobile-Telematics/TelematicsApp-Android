87a6276dd7237c33c078bc73a1f2a03880adca08
Ivan <bluetooth33d@gmail.com>
2021-07-05 14:46:08 +0300

    add Feed module

---
+++ features/feed/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    id(Plugins.androidLibrary)
+    id(Plugins.daggerHiltPlugin)
+    id(Plugins.kotlinAndroid)
+    id(Plugins.kotlinKapt)
+    id(Plugins.kotlinAndroidExtensions)
+}
+
+android {
+
+    compileSdkVersion(AppConfig.compileSdk)
+
+    defaultConfig {
+        //applicationId = "${AppConfig.applicationIdPrefix}.dashboard"
+        versionCode = AppConfig.versionCode
+        versionName = AppConfig.versionName
+        minSdkVersion(AppConfig.minSdk)
+        targetSdkVersion(AppConfig.targetSdk)
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            debuggable(false)
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            manifestPlaceholders(mapOf(Pair("crashlyticsCollectionEnabled", true)))
+            //signingConfig = signingConfigs.release
+        }
+        getByName("debug") {
+            isMinifyEnabled = false
+            debuggable(true)
+            manifestPlaceholders(mapOf(Pair("crashlyticsCollectionEnabled", false)))
+            //signingConfig = signingConfigs.debug
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    implementation(AppDependencies.appLibraries)
+
+    implementation(project(Modules.domain))
+    implementation(project(Modules.data))
+
+    implementation(AppDependencies.lifecycleKtx)
+    implementation(AppDependencies.daggerHiltLibraries)
+    kapt(AppDependencies.daggerHiltCompiler)
+    kapt(AppDependencies.daggerHiltAndroidXCompiler)
+}
