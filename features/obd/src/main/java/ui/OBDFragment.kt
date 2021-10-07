591b3ed2aa7f7b1b6701845a8cb78b77208d6bb5
Ivan <bluetooth33d@gmail.com>
2021-10-07 16:30:50 +0300

    add OBD module

---
+++ features/obd/src/main/java/ui/OBDFragment.kt
@@ -0,0 +1,28 @@
+package ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.telematics.content.utils.BaseFragment
+import com.telematics.reward.databinding.FragmentObdBinding
+
+class OBDFragment : BaseFragment() {
+
+    private lateinit var binding: FragmentObdBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentObdBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
